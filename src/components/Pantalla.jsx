df58e53de0f9783fe5466c83640fa9ab7b2ead79
Federico <federicogrunwald96@gmail.com>
2023-06-19 10:21:00 -0300

    calculator

---
+++ src/components/Pantalla.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../style/Pantalla.css"
+
+function Pantalla({input}) {
+  return (
+    <div className='input'>
+        {input}
+    </div>
+  )
+}
+
+export default Pantalla
